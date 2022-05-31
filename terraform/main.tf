70b2c23b58b6df8de573ed38014410382821a377
RalphBrynard <ralph@thebrynards.com>
2022-05-31 10:08:07 -0500

    Commit on main

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,6 @@ output "organization_id" {
 module "oauth_client" {
   source                = "BrynardSecurity-terraform/terraform-cloud/tfe//modules/tfe_oauth_client"
   version               = "0.0.9"
-  depends_on            = [module.organization]
   api_url               = var.api_url
   https_url             = var.https_url
   oauth_token           = var.github_pat_token
@@ -27,34 +26,4 @@ module "oauth_client" {
 
 output "oauth_client_id" {
   value = module.oauth_client.oauth_token_id
-}
-
-module "workspace" {
-  source                        = "BrynardSecurity-terraform/terraform-cloud/tfe//modules/tfe_workspace"
-  version                       = "0.0.9"
-  add_vcs_repo                  = true
-  agent_pool_id                 = ""
-  allow_destroy_plan            = false
-  auto_apply                    = true
-  execution_mode                = "remote"
-  file_triggers_enabled         = true
-  global                        = true
-  global_remote_state           = true
-  name                          = module.organization.tfe_organization_id
-  oauth_token_id                = module.oauth_client.oauth_token_id
-  organization                  = module.organization.tfe_organization_id
-  queue_all_runs                = true
-  remote_state_consumer_ids     = [""]
-  speculative_enabled           = true
-  structured_run_output_enabled = true
-  ssh_key_id                    = ""
-  terraform_version             = ""
-  tfe_token                     = var.terraform_api_token
-  trigger_prefixes              = ""
-  tags                          = ["prod"]
-  vcs_branch                    = "main"
-  vcs_repository                = ""
-  workspace_description         = ""
-  working_directory             = "./terraform"
-  workspace_variable            = true
 }
