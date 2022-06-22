84c14c40390fe1c1ccc2d6a23238af355989aeef
RalphBrynard <ralph@thebrynards.com>
2022-06-22 10:03:37 -0400

    pre-commit hooks

--- modules/tfe_oauth_client/main.tf
+++ modules/tfe_oauth_client/main.tf
@@ -1,14 +1,10 @@
-locals {
-  organization_name = lower(replace("${var.organization_name}", " ", "_"))
-}
-
 module "oauth_client" {
   source                = "BrynardSecurity-terraform/terraform-cloud/tfe//modules/tfe_oauth_client"
   version               = "0.1.5"
   api_url               = var.api_url
   https_url             = var.https_url
   oauth_token           = var.oauth_token
-  organization          = local.organization_name
+  organization          = var.organization_name
   service_provider      = var.service_provider
-  tfe_oauth_client_name = "Terraform Cloud - ${local.organization_name}"
+  tfe_oauth_client_name = "Terraform Cloud - ${var.organization_name}"
 }
